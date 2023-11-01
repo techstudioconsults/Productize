7f63ed6a00e699f242c7cf20b78d6ce27875ba40
Kingsley Solomon <kinxly@gmail.com>
2023-11-01 08:33:09 +0100

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="productize-v1.0.0" />
+    </div>
+  );
+}
+
+export default App;

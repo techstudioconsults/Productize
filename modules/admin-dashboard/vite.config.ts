43b4ea899ecff047b8cbafb9e98fc3ce88dbb794
Kingsley Solomon <kinxly@gmail.com>
2024-06-15 12:50:54 +0100

    fix: vite-config

--- modules/admin-dashboard/vite.config.ts
+++ modules/admin-dashboard/vite.config.ts
@@ -1,56 +1,81 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
     root: __dirname,
     cacheDir: '../../node_modules/.vite/modules/admin-dashboard',
 
-    plugins: [react(), nxViteTsPaths(), dts({ entryRoot: 'src', tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'), skipDiagnostics: true })],
-
-    // Uncomment this if you are using workers.
-    // worker: {
-    //  plugins: [ nxViteTsPaths() ],
-    // },
-
-    // Configuration for building your library.
-    // See: https://vitejs.dev/guide/build.html#library-mode
-    build: {
-        outDir: '../../dist/modules/admin-dashboard',
-        reportCompressedSize: true,
-        commonjsOptions: {
-            transformMixedEsModules: true,
-        },
-        lib: {
-            // Could also be a dictionary or array of multiple entry points.
-            entry: 'src/index.ts',
-            name: 'admin-dashboard',
-            fileName: 'index',
-            // Change this to the formats you want to support.
-            // Don't forget to update your package.json as well.
-            formats: ['es', 'cjs'],
-        },
-        rollupOptions: {
-            // External packages that should not be bundled into your library.
-            external: ['react', 'react-dom', 'react/jsx-runtime'],
-        },
-    },
+    plugins: [react(), nxViteTsPaths()],
 
     test: {
         globals: true,
-        cache: {
-            dir: '../../node_modules/.vitest',
-        },
+        cache: { dir: '../../node_modules/.vitest' },
         environment: 'jsdom',
         include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
+        setupFiles: './test/testSetup.ts',
         reporters: ['default'],
         coverage: {
             reportsDirectory: '../../coverage/modules/admin-dashboard',
             provider: 'v8',
+            enabled: true,
         },
     },
 });
+
+// /// <reference types='vitest' />
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import dts from 'vite-plugin-dts';
+// import * as path from 'path';
+// import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+// export default defineConfig({
+//     root: __dirname,
+//     cacheDir: '../../node_modules/.vite/modules/admin-dashboard',
+
+//     plugins: [react(), nxViteTsPaths(), dts({ entryRoot: 'src', tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'), skipDiagnostics: true })],
+
+//     // Uncomment this if you are using workers.
+//     // worker: {
+//     //  plugins: [ nxViteTsPaths() ],
+//     // },
+
+//     // Configuration for building your library.
+//     // See: https://vitejs.dev/guide/build.html#library-mode
+//     build: {
+//         outDir: '../../dist/modules/admin-dashboard',
+//         reportCompressedSize: true,
+//         commonjsOptions: {
+//             transformMixedEsModules: true,
+//         },
+//         lib: {
+//             // Could also be a dictionary or array of multiple entry points.
+//             entry: 'src/index.ts',
+//             name: 'admin-dashboard',
+//             fileName: 'index',
+//             // Change this to the formats you want to support.
+//             // Don't forget to update your package.json as well.
+//             formats: ['es', 'cjs'],
+//         },
+//         rollupOptions: {
+//             // External packages that should not be bundled into your library.
+//             external: ['react', 'react-dom', 'react/jsx-runtime'],
+//         },
+//     },
+
+//     test: {
+//         globals: true,
+//         cache: {
+//             dir: '../../node_modules/.vitest',
+//         },
+//         environment: 'jsdom',
+//         include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+//         reporters: ['default'],
+//         coverage: {
+//             reportsDirectory: '../../coverage/modules/admin-dashboard',
+//             provider: 'v8',
+//         },
+//     },
+// });

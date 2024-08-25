76ce0bdfcc2d73ac59eb675a8a6c3e3d8993ee0c
Kingsley Solomon <kinxly@gmail.com>
2024-08-25 16:18:29 +0100

    fix: resolved status not found error #124

--- modules/shared/redux/src/lib/products-state/productsApiSlice.js
+++ modules/shared/redux/src/lib/products-state/productsApiSlice.js
@@ -162,7 +162,6 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             async onQueryStarted(arg, { dispatch, queryFulfilled }) {
                 try {
                     const { data } = await queryFulfilled;
-                    console.log(data);
                     dispatch(
                         setAllProduct({
                             products: data.data,

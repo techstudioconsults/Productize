3743b6c75866b08467f3d3db4cbdf0b7e0f64fe5
Intuneteq <tobiolanitori@gmail.com>
2025-02-07 16:15:22 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
     plugins: [react(), nxViteTsPaths()],
 
     build: {
+        watch: true,
         outDir: './dist',
         reportCompressedSize: true,
         commonjsOptions: {
